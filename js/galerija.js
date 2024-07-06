9545084964b72756b8f00f9bd14eec9ad25b783f
djurke23 <106180427+djurke23@users.noreply.github.com>
2024-07-06 16:49:46 +0200

    Update galerija.js

--- js/galerija.js
+++ js/galerija.js
@@ -1,3 +1,35 @@
+// STRELICA SCROLL TO TOP - sve stranice
+
+
+var scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollTopBtn.classList.add("show");
+    } else {
+        scrollTopBtn.classList.remove("show");
+    }
+}
+
+scrollTopBtn.onclick = function() {
+    scrollToTop();
+}
+
+function scrollToTop() {
+    var currentPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (currentPosition > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, currentPosition - currentPosition / 8);
+    }
+}
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const navItems = document.querySelectorAll('.nav-item');
     const galleryItems = document.querySelectorAll('.gallery-item');
