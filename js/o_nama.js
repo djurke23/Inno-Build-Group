5e1c57e006d6afdd79ce224f09ec3eeb5f500cc4
djurke23 <106180427+djurke23@users.noreply.github.com>
2024-07-21 13:57:22 +0200

    Update o_nama.js

--- js/o_nama.js
+++ js/o_nama.js
@@ -44,3 +44,82 @@ document.querySelector('form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Hvala na pretplati!');
 });
+
+
+// timeline
+
+// VARIABLES
+const elH = document.querySelectorAll(".timeline li > div");
+const timeline = document.querySelector(".timeline ol");
+
+// START
+window.addEventListener("load", init);
+
+function init() {
+  setEqualHeights(elH);
+  addTouchScrolling();
+}
+
+// SET EQUAL HEIGHTS
+function setEqualHeights(el) {
+  let counter = 0;
+  for (let i = 0; i < el.length; i++) {
+    const singleHeight = el[i].offsetHeight;
+
+    if (counter < singleHeight) {
+      counter = singleHeight;
+    }
+  }
+
+  for (let i = 0; i < el.length; i++) {
+    el[i].style.height = `${counter}px`;
+  }
+}
+
+// ADD TOUCH SCROLLING
+function addTouchScrolling() {
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  timeline.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - timeline.offsetLeft;
+    scrollLeft = timeline.scrollLeft;
+  });
+
+  timeline.addEventListener('mouseleave', () => {
+    isDown = false;
+  });
+
+  timeline.addEventListener('mouseup', () => {
+    isDown = false;
+  });
+
+  timeline.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - timeline.offsetLeft;
+    const walk = (x - startX) * 2; // Scroll-fast
+    timeline.scrollLeft = scrollLeft - walk;
+  });
+
+  // Touch events for mobile
+  timeline.addEventListener('touchstart', (e) => {
+    isDown = true;
+    startX = e.touches[0].pageX - timeline.offsetLeft;
+    scrollLeft = timeline.scrollLeft;
+  });
+
+  timeline.addEventListener('touchend', () => {
+    isDown = false;
+  });
+
+  timeline.addEventListener('touchmove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.touches[0].pageX - timeline.offsetLeft;
+    const walk = (x - startX) * 2;
+    timeline.scrollLeft = scrollLeft - walk;
+  });
+}
