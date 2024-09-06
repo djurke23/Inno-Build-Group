6a6436cd88cbd55f1f5565b752bd665b88790d55
djurke23 <106180427+djurke23@users.noreply.github.com>
2024-09-06 15:25:55 +0200

    Add files via upload

--- form-handler.php
+++ form-handler.php
@@ -1,33 +1,32 @@
-
 <?php
 
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$name = htmlspecialchars(trim($_POST['name']));
+$visitor_email = htmlspecialchars(trim($_POST['email']));
+$subject = htmlspecialchars(trim($_POST['subject']));
+$message = htmlspecialchars(trim($_POST['message']));
 
-
-$email_from ='info@';  /* sajt kako ide   */ 
-
-$email_subject = 'Mew Form Sunmission';
-
+$email_from = 'info@innobuildgroup.rs'; 
+$email_subject = 'New Form Submission';
 
 $email_body = "User Name: $name. \n" .
-            "User Email: $visitor_email.\n".
-            "Subject: $subject.\n".
-            "User Message: $message •In";
+              "User Email: $visitor_email.\n" .
+              "Subject: $subject.\n" .
+              "User Message: $message.\n";
 
-$to = 'innobuildgroup23@gmail.com';
-
+$to = 'innobuildgroup00@gmail.com';
 $headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-$headers = "Reply-To: $visitor_email \r\n";
+if (!filter_var($visitor_email, FILTER_VALIDATE_EMAIL)) {
+    die("Invalid email format");
+}
 
-mail ($to,$email_subject, $email_body, $headers);
-
-header("Location: contact.html");
-
-
+if (mail($to, $email_subject, $email_body, $headers)) {
+    header("Location: kontakt.html");
+    exit();
+} else {
+    echo "Email sending failed";
+}
 
 
 ?>
